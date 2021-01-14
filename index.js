a5ee1ca8402342101fbd60ca2bb56b9bf822798e
Chi Nguyen <nguyen.thimai@northeastern.edu>
2021-01-13 23:40:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Add your code here
+
+// let submitData = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json",
+//   },
+//   body: JSON.stringify(userData),
+// };
+
+// fetch("http://localhost:3000/users", submitData)
+// .then(res => res.json())
+// .then(json => console.log(json))
+// .catch(function(error) {
+//    alert("Bad things! Ragnarők!");
+//    console.log(error.message);
+// })
+
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+    },
+    body: JSON.stringify({name, email}),
+  })
+    .then(res => res.json())
+    .then(function(user) {
+      document.body.innerHTML = user.id
+    })
+    .catch(function(error) {
+        alert("Bad things!");
+        document.body.innerHTML = error.message;
+  });
+}
